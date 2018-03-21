82e740bdd580e887ce61212cc1d0e23efc8c96d8
Danilov Boris <brdanilov@gmail.com>
2018-03-21 13:54:20 +0300

    start here

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 
-//hello
+enum class Cell : int {
+    blank = 0,
+    p1virus = 1,
+    p2virus = 2,
+    p1food = 3,
+    p2food = 4
+};
+
+const int N = 8;
+Cell field[N][N];
+
+void printfield() {
+
+};
+
+int main() {
+
+    return 1;
+}
+
