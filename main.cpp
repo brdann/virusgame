6a13d073f9c337d3617e4f9612b78238804aea8f
Liza <elizaveta-bogdan@mail.ru>
2018-03-21 14:37:29 +0300

    Add putcross

--- main.cpp
+++ main.cpp
@@ -13,6 +13,38 @@ enum class Cell : int
 const int N = 8;
 Cell field[N][N];
 
+void putcross(string command, int player)
+{
+    string s;
+    int i = 0;
+    int x=0;
+    int y=0;
+    while (command[i]>47 && s[i]<58)
+    {
+        s+=command[i];
+        i++;
+        x = stoi(s);
+    }
+    string s2;
+    if (command[i]==':')
+    {
+        while (command[i]>47 && s[i]<58)
+        {
+            s2+=command[i];
+            i++;
+        }
+        y = stoi(s2);
+    }
+    else
+    {
+        cout << "Wrong command" << endl;
+    }
+    if (player==1)
+        field[x][y]=Cell::p1virus;
+    else
+        field[x][y]=Cell::p2virus;
+}
+
 void printfield()
 {
     for (int i = 0; i < 10; i++)
