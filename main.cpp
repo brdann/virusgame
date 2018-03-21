b5b90ffbc4ff6aeae9aae1d05b542266cce8c4c4
Genda <zayaz50061reZ@mail.ru>
2018-03-21 14:13:47 +0300

    add fieldprint

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
-enum class Cell : int {
+enum class Cell : int
+{
     blank = 0,
     p1virus = 1,
     p2virus = 2,
@@ -11,12 +13,19 @@ enum class Cell : int {
 const int N = 8;
 Cell field[N][N];
 
-void printfield() {
-
+void printfield()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+            cout << ". ";
+        cout << "\n";
+    }
 };
 
-int main() {
-
+int main()
+{
+    printfield ();
     return 1;
 }
 
